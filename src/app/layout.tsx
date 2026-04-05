724c8708edb833a503cbb79cd9cf7f9211237df0
Tanya Elessen <you@elessenlabs.com>
2026-04-05 09:21:44 +0300

    Add Google Analytics (G-X5S5S1HR9S)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import AppShell from "./AppShell";
 
+
 export const metadata: Metadata = {
   title: "Elessen Labs",
   description:
@@ -25,11 +26,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://assets.calendly.com/assets/external/widget.css"
-        />
-      </head>
+  <link
+    rel="stylesheet"
+    href="https://assets.calendly.com/assets/external/widget.css"
+  />
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-X5S5S1HR9S"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      window.gtag = gtag;
+      gtag('js', new Date());
+      gtag('config', 'G-X5S5S1HR9S');
+    `}
+  </Script>
+</head>
       <body className="min-h-screen bg-white text-gray-900">
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XXXX"
